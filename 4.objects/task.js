f0a61c514ba81b2b86ebcda9139a64a5f82f94bc
NK Test <nkamelin@gmail.com>
2023-04-05 20:01:43 +0300

    4е задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
+let student3 = new Student("Oleg", "мужской", 34);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+	if (this.hasOwnProperty("marks")) {
+		this.marks = [...this.marks, ...marksToAdd];
+	}
 }
 
 Student.prototype.getAverage = function () {
-  
+	if (this.hasOwnProperty("marks")) {
+		return this.marks.reduce((acc, mark) =>  acc + mark / this.marks.length, 0);
+	}
+	else {
+		return 0;
+	}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
 }
+
