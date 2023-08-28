b41a0b4cb4a9fbaf4ea45be2a993efdd74b39c0a
NK Test <nkamelin@gmail.com>
2023-08-28 17:40:47 +0300

    3.arrays - done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length == arr2.length ?
+		arr1.every((item, index) => item === arr2[index]): 
+		false;
 }
 
+// среднее значение возраста пользователей одного пола.
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let arr = users.filter(user => user.gender === gender).map(item => item.age);
+	return arr.reduce((acc, current) => (acc + current/arr.length), 0)
 }
+
+ 
--- 5.classes/task.js
+++ 5.classes/task.js
@@ -71,25 +71,50 @@ class Library {
 		}
 	}
 	findBookBy(type, value) {
-		let result = this.books.find((item, index) => item[type] === value);
+		let result = this.books.find(book => book[type] === value);
 
-		if (result) {
-			return result;
-		} else {
-			return null;
-		}
+		return result ? result: null;
 	}
 	giveBookByName(bookName) {
-		let findIndex = this.books.findIndex((item, index) => item.name === bookName);
+		let findIndex = this.books.findIndex(book => book.name === bookName);
 
-		if (findIndex !== -1) {
-			return this.books.splice(findIndex, 1)[0]; //Ошибка исправлена
-		} else {
-			return null;
-		}
-			
-	}
+		return findIndex !== -1 ? this.books.splice(findIndex, 1)[0]: null;
+	}	
 }
+
+
+
+
+// class Library {
+// 	constructor(name) {
+// 		this.name = name;
+// 		this.books = [];
+// 	}
+// 	addBook(book) {
+// 		if (book.state > 30) {
+// 			this.books.push(book);
+// 		}
+// 	}
+// 	findBookBy(type, value) {
+// 		let result = this.books.find((item, index) => item[type] === value);
+
+// 		if (result) {
+// 			return result;
+// 		} else {
+// 			return null;
+// 		}
+// 	}
+// 	giveBookByName(bookName) {
+// 		let findIndex = this.books.findIndex((item, index) => item.name === bookName);
+
+// 		if (findIndex !== -1) {
+// 			return this.books.splice(findIndex, 1)[0]; //Ошибка исправлена
+// 		} else {
+// 			return null;
+// 		}
+			
+// 	}
+// }
 
 ///* -----------------------------------------------------------------------------//
 
@@ -137,29 +162,29 @@ student.addMark(5, "физика");
 
 ///?-----------------------------------------------------------------------------//
 
-// const library = new Library("Библиотека имени Ленина");
+const library = new Library("Библиотека имени Ленина");
 
-// library.addBook(
-//  new DetectiveBook(
-//    "Артур Конан Дойл",
-//    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-//    2019,
-//    1008
-//  )
-// );
-// library.addBook(
-//  new FantasticBook(
-//    "Аркадий и Борис Стругацкие",
-//    "Пикник на обочине",
-//    1972,
-//    168
-//  )
-// );
-// library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
-// library.addBook(new Magazine("Мурзилка", 1924, 60));
+library.addBook(
+ new DetectiveBook(
+   "Артур Конан Дойл",
+   "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+   2019,
+   1008
+ )
+);
+library.addBook(
+ new FantasticBook(
+   "Аркадий и Борис Стругацкие",
+   "Пикник на обочине",
+   1972,
+   168
+ )
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
 
 //console.log(library.giveBookByName("Мурзилка"));
-//library.findBookBy("releaseDate", 1924); 
+// library.findBookBy("releaseDate", 1924); 
 
 
 
